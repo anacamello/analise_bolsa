93836dfd2e6af553a5c23a19330be603e2fe238e
anacamello <ana.camello@gmail.com>
2023-01-27 09:45:24 -0300

    Update 1_Tendências.py

--- pages/1_Tendências.py
+++ pages/1_Tendências.py
@@ -232,11 +232,11 @@ def calcula_tendencia(modelo):
                     
                     
 
-    st.sidebar.success('Tendências de mínima/ máxima calculadas.')
+        st.sidebar.success('Tendências de mínima/ máxima calculadas.')
 
-    dados_consolidados = dados_consolidados.reset_index(drop=True)
+        dados_consolidados = dados_consolidados.reset_index(drop=True)
 
-    return dados_consolidados
+        return dados_consolidados
 
 
 # In[ ]:
