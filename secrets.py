2210af524ca7baeeb16f15b6069b79e5a7c0121e
anacamello <ana.camello@gmail.com>
2023-10-31 17:23:23 -0300

    Create secrets.py

---
+++ secrets.py
@@ -0,0 +1,36 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "e4ceb6a9-042f-40c0-b2bd-f27fbb6bcf18",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "api_key = 'LVPuYJqB9rPdBNfMwjJrfYjJaDaO6mkfKTEC4xKeisrVDVNQwcd5kdnhXlLFr4jf'\n",
+    "api_secret = 'NP08r4Sff9vQfkdnO4MHa6SwOf8VWvJwBEidPor58tDmRP7yLBTJ46qNwlykqCky'"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.9.13"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
